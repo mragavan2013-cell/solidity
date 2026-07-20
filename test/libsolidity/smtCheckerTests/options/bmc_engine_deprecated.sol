124c9414ffaf76ab5a324bb4918b45fa6ac36e8a
Matheus Aguiar <matheus.pit@gmail.com>
2026-07-20 18:42:01 -0300

    Add test for bmc deprecation warning

---
+++ test/libsolidity/smtCheckerTests/options/bmc_engine_deprecated.sol
@@ -0,0 +1,7 @@
+contract C {
+	function f() public pure {}
+}
+// ====
+// SMTEngine: bmc
+// ----
+// Warning 3993: The BMC engine of the SMTChecker is deprecated and will be removed in a future release.Please use the CHC engine instead.
